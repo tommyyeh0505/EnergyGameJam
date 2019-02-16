7a0f8e840a0074945b0733daeb9111e2598c99d1
Russell Blickhan <rwblickhan@gmail.com>
2019-02-16 13:30:09 -0800

    Add bullet prefab

---
+++ Assets/Scripts/BulletBehaviourScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletBehaviourScript : MonoBehaviour
+{
+    private Rigidbody2D body;
+
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (body)
+        {
+            body.MovePosition(body.GetRelativePoint(new Vector2(0, 0.5f)));
+        }
+    }
+}
---
+++ Assets/Scripts/ShipGunComponent.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipGunComponent : MonoBehaviour
+{
+    [SerializeField] public GameObject prefabBullet;
+    private ShipEnergyComponent energyComponent;
+
+    void Start()
+    {
+        energyComponent = GetComponent<ShipEnergyComponent>();        
+    }
+
+    void Update()
+    {
+        if (Input.GetButton("shoot"))
+        {
+            Instantiate(prefabBullet);
+        }
+    }
+}
