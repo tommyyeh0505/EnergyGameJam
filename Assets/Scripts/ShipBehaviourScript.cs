f9926ac212bf2fdc9da3976eb320e1a1bf767da6
jiahuahl <jiahuahlee@hotmail.com>
2019-02-16 00:14:26 -0800

    make input play nice with gravity, add reset button. tweaking initial movement values

--- Assets/Scripts/ShipBehaviourScript.cs
+++ Assets/Scripts/ShipBehaviourScript.cs
@@ -2,16 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class ShipBehaviourScript : MonoBehaviour {
 
 	public float speed = 3f;
     public float rotateSpeed = 0.1f;   // degrees per second
     private Vector3 moveDirection = Vector3.zero;
-    private CharacterController controller;
+    private Rigidbody2D rgbd2d;
 
 	// Use this for initialization
 	void Start () {
-        controller = GetComponent<CharacterController>();
+        rgbd2d = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
@@ -19,7 +20,13 @@ public class ShipBehaviourScript : MonoBehaviour {
         moveDirection = new Vector3(0, Input.GetAxis("Vertical"), 0);
         moveDirection = transform.TransformDirection(moveDirection);
 
-        transform.Rotate(0, 0, Input.GetAxis("Horizontal") * rotateSpeed);
-        controller.Move(moveDirection * speed * Time.deltaTime);
+        transform.Rotate(0, 0, -Input.GetAxis("Horizontal") * rotateSpeed);
+        rgbd2d.AddForce(moveDirection * speed * Time.deltaTime);
+
+        if (Input.GetButtonDown("reset"))
+        {
+            transform.position = new Vector3(Camera.main.transform.position.x, Camera.main.transform.position.y, 0.0f);
+            rgbd2d.velocity = Vector3.zero;
+        }
 	}
 }
