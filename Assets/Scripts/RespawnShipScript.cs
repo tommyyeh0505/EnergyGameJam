51a2187c9bf009e8ffd59c88fe5227c3440deed8
jiahuahl <jiahuahlee@hotmail.com>
2019-02-16 02:40:15 -0800

    add respawning after death (press r)

---
+++ Assets/Scripts/RespawnShipScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RespawnShipScript : MonoBehaviour
+{
+    public GameObject shipType;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("reset") && shipType)
+        {
+            GameObject ship = GameObject.FindGameObjectWithTag("Player");
+            if (!ship)
+            {
+                Instantiate(shipType, Vector3.zero, Quaternion.identity);
+            }
+        }
+    }
+}
