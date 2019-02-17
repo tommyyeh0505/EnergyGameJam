13115dc696f2e8e1deea890fa9ea28a0e689f05e
Russell Blickhan <rwblickhan@gmail.com>
2019-02-16 17:27:00 -0800

    Initial implementation of energy pickup

---
+++ Assets/Scripts/EnergyPickupBehaviourScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnergyPickupBehaviourScript : MonoBehaviour
+{
+    private GameObject ship;
+    [SerializeField] public float energyGain = 10.0f;
+    [SerializeField] public float attractDistance = 10.0f;
+    [SerializeField] public float attractSpeed = 5f;
+
+    void Start()
+    {
+        ship = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void Update()
+    {
+        if (ship) {
+            Vector2 shipPos = ship.transform.position;
+            Vector2 pos = gameObject.transform.position;
+            if (Vector2.Distance(shipPos, pos) < attractDistance)
+            {
+                Vector2 dir = (shipPos - pos).normalized;
+                gameObject.transform.Translate(attractSpeed * dir * Time.deltaTime);
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            ShipEnergyComponent energyComponent = col.gameObject.GetComponent<ShipEnergyComponent>();
+            if (energyComponent)
+            {
+                energyComponent.GainEnergy(energyGain);
+            }
+            Destroy(this.gameObject);
+        }
+    }
+}
